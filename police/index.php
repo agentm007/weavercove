2e8eafd32aa53935389e21cf6484f6a23d19aedf
adexter <adexter@accertify.com>
2018-05-21 13:32:25 -0500

    Seperated subdomains

---
+++ police/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Weaver Cove Board of Tourism</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Weaver Cove RPG game site">
+    <meta name="keywords" content="RPG, City Planning, Library">
+
+    <script src="js/popper.min.js"></script>
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  </head>
+
+  <body>
+  </body>
+
+</html>
