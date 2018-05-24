599129a7c9565bdce9f35f52757b7e10d975cec2
adexter <adexter@accertify.com>
2018-05-23 19:37:33 -0500

    updated folder structure

--- pubLibrary/index.php
+++ pubLibrary/index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Weaver Cove Board of Tourism</title>
+    <title>Weaver Cove Public Library</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Weaver Cove RPG game site">
